0867ecf39067ddae3d8cca7b5d0383c6a0674324
smkankanamge <119028384+mSrishan@users.noreply.github.com>
2025-04-12 21:51:12 +0530

    update dotenv

--- database/migrations/2025_04_06_160212_create_students_table.php
+++ database/migrations/2025_04_06_160212_create_students_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("name");
+            $table->string("phone");
+            $table->string("email")->nullable();
+            $table->timestamps(); //created_at
         });
     }
 
